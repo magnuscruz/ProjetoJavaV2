f8e0d49135c074050214a61eb6ed74631d519687
Taís Bellaver <taisbellaver@gmail.com>
2021-01-01 11:36:50 +0000

    Inseri novamente comentários da classe Main Interface, Interface, util e Text.properties para Adriano testar

--- src/com/company/MainInterface.java
+++ src/com/company/MainInterface.java
@@ -1,16 +1,16 @@
-
-
-package com.company;
-
-
-import com.company.gui.Interface;
-
-public class MainInterface {
-
-    public static void main(String[] args) {
-        Interface sistema = new Interface();
-        sistema.setVisible(true);
-    }
-
-}
+//
+//
+//package com.company;
+//
+//
+//import com.company.gui.Interface;
+//
+//public class MainInterface {
+//
+//    public static void main(String[] args) {
+//        Interface sistema = new Interface();
+//        sistema.setVisible(true);
+//    }
+//
+//}
 
